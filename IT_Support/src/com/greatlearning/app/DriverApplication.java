df3b576a50f58539a0ff7de36dff9ce9ed3dc64a
D-eviloper <dhir.xcess@gmail.com>
2022-03-13 19:39:52 +0530

    minor changes in switch case

--- IT_Support/src/com/greatlearning/app/DriverApplication.java
+++ IT_Support/src/com/greatlearning/app/DriverApplication.java
@@ -6,37 +6,36 @@ package com.greatlearning.app;
 import com.greatlearning.model.Company;
 import com.greatlearning.model.Employee;
 import com.greatlearning.services.CredentialService;
+import com.greatlearning.services.ServiceExceptions;
+
 import java.util.Scanner;
 
 /**
  * @author Rahul Dubey
+ * @date 03-13-2022
  *
  */
 
-
 public class DriverApplication {
-	
+
 	/**
-	 * Data members: 
-	 * - String firstName : To store the firstName. 
-	 * - String lastName : To store the lastName 
-	 * - Scanner sc : To get user input using java.util.Scanner;
-	 * Constructor: to intialize the Scanner sc object for user input
-	 * Methods: 
-	 * - static void main(String[] args) : Main method to drive the application 
-	 * - void fetchEmployeeDB(): To fetch the Employee List of the company 
-	 * - void addCredentials(int option): To generate email,password of the employee
+	 * Data members: - String firstName : To store the firstName. - String lastName
+	 * : To store the lastName - Scanner sc : To get user input using
+	 * java.util.Scanner; Constructor: to intialize the Scanner sc object for user
+	 * input Methods: - static void main(String[] args) : Main method to drive the
+	 * application - void fetchEmployeeDB(): To fetch the Employee List of the
+	 * company - void addCredentials(int option): To generate email,password of the
+	 * employee
 	 *
 	 */
-	
+
 	private String firstName = "";
 	private String lastName = "";
 	private Scanner sc;
-	
-	
 
 	/**
-	 * Default constructor to initialize the Scanner object once and to use it across the class
+	 * Default constructor to initialize the Scanner object once and to use it
+	 * across the class
 	 */
 
 	public DriverApplication() {
@@ -44,69 +43,75 @@ public class DriverApplication {
 	}
 
 	/**
-	 * addCredentials(int option) : it takes the user input and generate the email and password respective to their department.
-	 * @ params option: user-input integer value respective to its departments ex: 1 for Technical, 2 for Admin etc
-	 * @ features: 
-	 * 		1. User Input
-	 * 		2. User choice for department
-	 * 	 	3. Generation of email Address
-	 * 		4. Generation of password
+	 * addCredentials(int option) : it takes the user input and generate the email
+	 * and password respective to their department. @ params option: user-input
+	 * integer value respective to its departments ex: 1 for Technical, 2 for Admin
+	 * etc @ features: 1. User Input 2. User choice for department 3. Generation of
+	 * email Address 4. Generation of password
 	 */
 	public void addCredentials(int option) {
-		// user input for his First name and last name
-		System.out.println("Please enter your First Name:");
-		firstName = sc.next();
-		System.out.println("Please enter your Last Name:");
-		lastName = sc.next();
-		
-		// Employee object to store the first name and last name
-		Employee emp = new Employee(firstName, lastName);
-		
+
 		// Choice for the department according to the option that user had choosen
 		String department_choice = "";
-		switch (option) {
-		case 1:
-			department_choice = "Technical";
-			break;
-		case 2:
-			department_choice = "Admin";
-			break;
-		case 3:
-			department_choice = "Human Resource";
-			break;
-		case 4:
-			department_choice = "Legal";
-			break;
-		default:
-			System.out.println("Invalid option!! Please try again...");
-		}
+		try {
+			switch (option) {
+			case 0:
+				throw new ServiceExceptions("Thanks for using this application...");
+				
+			case 1:
+				department_choice = "Technical";
+				break;
+			case 2:
+				department_choice = "Admin";
+				break;
+			case 3:
+				department_choice = "Human Resource";
+				break;
+			case 4:
+				department_choice = "Legal";
+				break;
+			default:
+				throw new ServiceExceptions("Invalid option!! Please try again...");
 
-		// CredentialService object to create an object
-		CredentialService creds = new CredentialService();
+			}
+			// user input for his First name and last name
+			System.out.println("Please enter your First Name:");
+			firstName = sc.next();
+			System.out.println("Please enter your Last Name:");
+			lastName = sc.next();
 
-		// fetching the status of email already exists (false) or not (true)
-		boolean status = creds.generateEmailAddress(emp, department_choice);
-		if (status) {
-			creds.generatePassword(emp);
-			creds.showCredentials(emp);
+			// Employee object to store the first name and last name
+			Employee emp = new Employee(firstName, lastName);
+			// CredentialService object to create an object
+			CredentialService creds = new CredentialService();
+
+			// fetching the status of email already exists (false) or not (true)
+			boolean status = creds.generateEmailAddress(emp, department_choice);
+			if (status) {
+				creds.generatePassword(emp);
+				creds.showCredentials(emp);
+			}
+		} catch (ServiceExceptions e) {
+			System.out.println(e);
 		}
 
 	}
-	
+
 	/**
-	 *  it fetches the List of all employee whose email and password is already created
+	 * it fetches the List of all employee whose email and password is already
+	 * created
 	 */
-	
-	public void fetchEmployeeDB()
-	{
+
+	public void fetchEmployeeDB() {
 		Company.getEmployeeDatabase();
-		System.out.println("Thanks!!");
+		
 	}
-	
+
 	/**
-	 *  Main method, Execution will start from here and this is the starting point of the program
+	 * Main method, Execution will start from here and this is the starting point of
+	 * the program
 	 */
-	
+
 	public static void main(String[] args) {
 		DriverApplication driver = new DriverApplication();
 
@@ -120,12 +125,11 @@ public class DriverApplication {
 			System.out.println("press 0 to Exit");
 
 			option = driver.sc.nextInt();
-			if (option != 0)
-				driver.addCredentials(option);
+			driver.addCredentials(option);
 
 		} while (option != 0);
+
 		
-		System.out.println("List of onboarded Employees: ");
 		/*
 		 * Added Extra logic to get the Employee Database that we created for all user.
 		 */
