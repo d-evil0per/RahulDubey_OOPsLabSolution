91e774110a6112b9b225a664f95d3a8540abd0ee
D-eviloper <dhir.xcess@gmail.com>
2022-03-13 18:45:29 +0530

    updated DriverApplication code

--- IT_Support/src/com/greatlearning/app/DriverApplication.java
+++ IT_Support/src/com/greatlearning/app/DriverApplication.java
@@ -1,37 +1,68 @@
 /**
- * 
+ *  Driver Program that provides user interaction and Main program which drives problem statement
  */
 package com.greatlearning.app;
 
 import com.greatlearning.model.Company;
 import com.greatlearning.model.Employee;
 import com.greatlearning.services.CredentialService;
-
 import java.util.Scanner;
 
 /**
  * @author Rahul Dubey
  *
  */
+
+
 public class DriverApplication {
+	
+	/**
+	 * Data members: 
+	 * - String firstName : To store the firstName. 
+	 * - String lastName : To store the lastName 
+	 * - Scanner sc : To get user input using java.util.Scanner;
+	 * Constructor: to intialize the Scanner sc object for user input
+	 * Methods: 
+	 * - static void main(String[] args) : Main method to drive the application 
+	 * - void fetchEmployeeDB(): To fetch the Employee List of the company 
+	 * - void addCredentials(int option): To generate email,password of the employee
+	 *
+	 */
+	
 	private String firstName = "";
 	private String lastName = "";
 	private Scanner sc;
+	
+	
 
 	/**
-	 * @param args
+	 * Default constructor to initialize the Scanner object once and to use it across the class
 	 */
 
 	public DriverApplication() {
 		this.sc = new Scanner(System.in);
 	}
 
+	/**
+	 * addCredentials(int option) : it takes the user input and generate the email and password respective to their department.
+	 * @ params option: user-input integer value respective to its departments ex: 1 for Technical, 2 for Admin etc
+	 * @ features: 
+	 * 		1. User Input
+	 * 		2. User choice for department
+	 * 	 	3. Generation of email Address
+	 * 		4. Generation of password
+	 */
 	public void addCredentials(int option) {
+		// user input for his First name and last name
 		System.out.println("Please enter your First Name:");
 		firstName = sc.next();
 		System.out.println("Please enter your Last Name:");
 		lastName = sc.next();
+		
+		// Employee object to store the first name and last name
 		Employee emp = new Employee(firstName, lastName);
+		
+		// Choice for the department according to the option that user had choosen
 		String department_choice = "";
 		switch (option) {
 		case 1:
@@ -50,8 +81,10 @@ public class DriverApplication {
 			System.out.println("Invalid option!! Please try again...");
 		}
 
+		// CredentialService object to create an object
 		CredentialService creds = new CredentialService();
 
+		// fetching the status of email already exists (false) or not (true)
 		boolean status = creds.generateEmailAddress(emp, department_choice);
 		if (status) {
 			creds.generatePassword(emp);
@@ -60,11 +93,19 @@ public class DriverApplication {
 
 	}
 	
+	/**
+	 *  it fetches the List of all employee whose email and password is already created
+	 */
+	
 	public void fetchEmployeeDB()
 	{
 		Company.getEmployeeDatabase();
 		System.out.println("Thanks!!");
 	}
+	
+	/**
+	 *  Main method, Execution will start from here and this is the starting point of the program
+	 */
 	
 	public static void main(String[] args) {
 		DriverApplication driver = new DriverApplication();
@@ -84,6 +125,7 @@ public class DriverApplication {
 
 		} while (option != 0);
 		
+		System.out.println("List of onboarded Employees: ");
 		/*
 		 * Added Extra logic to get the Employee Database that we created for all user.
 		 */
